e5cf78ea87763e4dd56dee83f46a65fec732ef6e
jlgrons <j.gronsbell@utoronto.ca>
2021-04-06 10:43:24 -0400

    Example analysis file.

---
+++ example_analysis.R
@@ -0,0 +1,12 @@
+## Step 1: Generate the data
+n_lab <- 100
+n_unlab <- 20000
+p <- 10
+rho <- 0.3
+model_specification <- 'gaussian mixture'
+num_strata <- 2
+
+my_dat <- DataGeneration(n_lab, n_unlab, p, rho,
+                         signal = c(1, 1, 0.5, 0.,5),
+                         model_specification = model_specification,
+                         num_strata = num_strata)
