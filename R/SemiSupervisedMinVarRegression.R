0a794a1e3a6f6e88ee7b031cdf22d3456eb9baa7
jlgrons <j.gronsbell@utoronto.ca>
2021-04-19 13:15:21 -0400

    Clean function.

--- R/SemiSupervisedMinVarRegression.R
+++ R/SemiSupervisedMinVarRegression.R
@@ -1,17 +1,34 @@
-# computes the component-wise minimum variance estimator based on the CV resids
-min.var.est = function(beta.ssl, beta.sl, resids.gamma, resids.beta.sl,
-                       Xt, Xv, eps = NULL){
-  Xv.1 = cbind(1, rbind(Xt,Xv)); Xt.1 = cbind(1,Xt);
-  n.v = nrow(Xv.1); n.t = nrow(Xt.1); ones = c(1,1);
-  p = ncol(Xt); weight = matrix(NA, p +1, 2);
+# Updated: 2021-04-19
+
+#' Component-wise minimum variance semi-supervised regression.
+#'
+#' @param beta_SSL Semi-supervised
+#' @param beta_SL Basis matrix for unlabeled data set.
+#' @param resids_imp Basis matrix for labeled data set.
+#' @param resids_beta_SL Basis matrix for unlabeled data set.
+#' @param X_labeled Covariate matrix for labeled data set.
+#' @param X_unlabeled Covariate matrix for unlabeled data set.
+#' @param y Numeric outcome vector.
+#' @param samp_prob Numeric vector of weights.
+#' @param lambda Penalization parameter for initial ridge estimator.
+#' @export
+#' @return Vector containing regression coefficients.
+#'
+
+min.var.est = function(beta_SSL, beta_SL, resids_imp, resids_beta_SL,
+                       X_labeled, X_unlabeled, eps = NULL){
+
+  X_unlabeled.1 = cbind(1, rbind(X_labeled,X_unlabeled)); X_labeled.1 = cbind(1,X_labeled);
+  n.v = nrow(X_unlabeled.1); n.t = nrow(X_labeled.1); ones = c(1,1);
+  p = ncol(X_labeled); weight = matrix(NA, p +1, 2);
 
   if(is.null(eps)){eps = rep(0, p+1)};
 
   # compute minimum var estimator (note the residuals have been divided by mean of weights)
 
-  A = solve(t(Xv.1) %*% (Xv.1*c(dg.logit(Xv.1%*%beta.ssl))))*n.v;
-  T_1 = A %*% t(Xt.1*c(resids.gamma));
-  T_2 = A %*% t(Xt.1*c(resids.beta.sl));
+  A = solve(t(X_unlabeled.1) %*% (X_unlabeled.1*c(dg.logit(X_unlabeled.1%*%beta_SSL))))*n.v;
+  T_1 = A %*% t(X_labeled.1*c(resids_imp));
+  T_2 = A %*% t(X_labeled.1*c(resids_beta_SL));
 
   for (i in 1:(p + 1)){
     mat = cbind(T_1[i,], T_2[i,]);
@@ -26,16 +43,16 @@ min.var.est = function(beta.ssl, beta.sl, resids.gamma, resids.beta.sl,
   }
 
   # min var estimator
-  beta.ssl.w = beta.ssl*weight[,1] + weight[,2]*beta.sl
+  beta_SSL.w = beta_SSL*weight[,1] + weight[,2]*beta_SL
 
   # std error of min var
-  A = solve(t(Xv.1) %*% (Xv.1*c(dg.logit(Xv.1%*%beta.ssl.w))))*n.v;
-  w.beta.ssl = diag(weight[,1]); w.beta.sl =  diag(weight[,2]);
-  resids.w = (w.beta.ssl %*% t(Xt.1*c(resids.gamma)) +
-                w.beta.sl %*% t(Xt.1*resids.beta.sl))/n.t
+  A = solve(t(X_unlabeled.1) %*% (X_unlabeled.1*c(dg.logit(X_unlabeled.1%*%beta_SSL.w))))*n.v;
+  w.beta_SSL = diag(weight[,1]); w.beta_SL =  diag(weight[,2]);
+  resids.w = (w.beta_SSL %*% t(X_labeled.1*c(resids_imp)) +
+                w.beta_SL %*% t(X_labeled.1*resids_beta_SL))/n.t
   se.beta.w = sqrt(diag(A %*% (resids.w %*% t(resids.w)) %*% A))
 
 
-  return(list(beta = beta.ssl.w, weight= weight, se.est = se.beta.w))
+  return(list(beta = beta_SSL.w, weight= weight, se.est = se.beta.w))
 }
 
