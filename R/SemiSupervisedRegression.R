29e027cdef307ac606d80e100adcc4e03e07a848
jlgrons <j.gronsbell@utoronto.ca>
2021-04-19 12:09:10 -0400

    Clean up semi-supervised regression function.

--- R/SemiSupervisedRegression.R
+++ R/SemiSupervisedRegression.R
@@ -1,58 +1,54 @@
-# fits the glm using unlabeled and labeled data & computes corresponding supervised estimator
-glm.fit.SS = function(basis.x, Xt, Xv, Yt, samp.prob, lambda0 = NULL){
+# Updated: 2021-04-19
 
-  ## basis.x: basis matrix with first n.t rows corr to labeled data and unlabeled rows n.t+1 to n.t+n.v
-  ## Xt: covariates for labeled
-  ## Xt: covariates for labeled
-  ## Yt: labels
-  ## samp.prob: vector of sampling weights
-  ## lambda0: ridge parameter for SS estimator
+#' Semi-supervised regression.
+#'
+#' @param basis_labeled Basis matrix for labeled data set.
+#' @param bais_unlabeled Basis matrix for unlabeled data set.
+#' @param X_labeled Covariate matrix for labeled data set.
+#' @param X_unlabeled Covariate matrix for unlabeled data set.
+#' @param y Numeric outcome vector.
+#' @param samp_prob Numeric vector of weights.
+#' @param lambda Penalization parameter for initial ridge estimator.
+#' @export
+#' @return Vector containing regression coefficients.
+#'
 
-  n.t = length(Yt); pp = ncol(basis.x); p = ncol(Xt); ind.lab = 1:n.t;
+SemiSupervisedRegression <- function(basis_labeled, basis_unlabaled, X_labeled,
+                                     X_unlabeled, y, samp_prob, lambda = NULL){
 
-  if(is.null(lambda0)){lambda0 = log(pp)/n.t^1.5};
+  num_labeled <- length(y)
+  p_basis <- ncol(basis_labeled)
+  p_X <- ncol(X_labeled)
 
-  weights = 1/samp.prob/mean(1/samp.prob)
+  # Default lambda if nothing input by the user.
+  if(is.null(lambda)){lambda <- log(p_basis)/num_labeled^1.5}
 
-  # step 1: basis function regression
-  gamma = my.ridge(basis.x[ind.lab, ], Yt, weights = weights, lambda = lambda0);
+  # Standardized weights.
+  weights <- 1/samp_prob/mean(1/samp_prob)
 
-  # step 2: fit the SS GLM
-  imps.basis = g.logit(cbind(1, basis.x) %*% gamma);
-  dat.all = rbind(Xt, Xv);
-  beta.ssl = tryCatch(glm(imps.basis ~ dat.all,
-                          family = 'binomial')$coeff, error = function(e) rep(NA, p+1))
+  # Step 1: Basis function regression for imputation.
+  beta_imp <- my.ridge(basis_labeled, y, weights = weights, lambda = lambda)
 
-  # fit supervised estimate for comparison
-  beta.sl = tryCatch(glm(Yt ~ Xt, family = 'binomial',
-                         weights = weights)$coeff, error = function(e) rep(NA, p+1));
+  # Step 2: Semi-supervised regression using the imputations from Step 1.
+  basis_all <- rbind(basis_labeled, basis_unlabeled)
+  X_all <- rbind(X_labeled, X_unlabeled)
+  basis_imps <- g.logit(cbind(1, basis_all) %*% beta_imp);
 
-  beta.naive = tryCatch(glm(Yt ~ Xt, family = 'binomial')$coeff,
-                        error = function(e) rep(NA, p+1));
+  beta_SSL <- tryCatch(glm(basis_imps ~ X_all,
+                          family = 'binomial')$coeff,
+                      error = function(e) rep(NA, p_X+1))
 
-  # Fit the density ratio estimator
+  # Fit supervised estimate for comparison.
+  beta_SL <- tryCatch(glm(y ~ X_labeled, family = 'binomial',
+                         weights = weights)$coeff,
+                     error = function(e) rep(NA, p_X+1))
 
-  n.v <- length(basis.x[,1])
-  basis.phi <- cbind(rep(1, n.v), basis.x)
-  phi.t <- basis.phi[1:n.t, ]
-  phi.v <- basis.phi[- c(1:n.t), ]
-  dim.basis <- length(basis.phi[1, ])
+  # Fit supervised estimate, without weights, for comparison.
+  beta_SL_unweighted <- tryCatch(glm(y ~ X_labeled, family = 'binomial')$coeff,
+                        error = function(e) rep(NA, p_X+1))
 
-  phiT.phi <- t(basis.phi) %*% basis.phi / (n.t + n.v)
-  E.phi <- t(phi.t) %*% weights / n.t
-  theta.ratio <- solve(phiT.phi + diag(rep(0.01, ncol(phiT.phi)))) %*% (rowMeans(t(phi.v)) - E.phi)
-  density.ratio <- exp(phi.t %*% theta.ratio)
-  weights.dr <- density.ratio * weights
 
-  beta.dr <- tryCatch(glm(Yt~Xt, family = 'binomial',
-                          weights = weights.dr)$coeff, error = function(e) rep(NA, p+1));
-
-  u.dr <- diag(as.vector(Yt - g.logit(cbind(1, Xt) %*% beta.dr))) %*% cbind(1, Xt)
-  E.uT.phi <- t(u.dr) %*% diag(as.vector(weights)) %*% phi.t / sum(weights)
-  proj.coef.dr <- E.uT.phi %*% solve(phiT.phi + diag(rep(0.01, ncol(phiT.phi))))
-  proj.dr <- diag(as.vector(weights)) %*% phi.t %*% t(proj.coef.dr)
-
-  return(list(beta.ssl = beta.ssl, beta.sl = beta.sl, gamma = gamma,
-              beta.dr = beta.dr, proj.dr = proj.dr,
-              beta.naive = beta.naive))
+  return(list(beta_SSL = beta_SSL, beta_SL = beta_SL,
+              beta_SL_unweighted = beta_SL_unweighted,
+              beta_imp = beta_imp, lambda = lambda))
 }
