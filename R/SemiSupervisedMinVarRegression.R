3792b7c8324da3b9c555c684c3b47b675f936ef6
jlgrons <j.gronsbell@utoronto.ca>
2021-04-19 13:32:12 -0400

    Yikes with the equals signs.

--- R/SemiSupervisedMinVarRegression.R
+++ R/SemiSupervisedMinVarRegression.R
@@ -13,29 +13,31 @@
 #' @return List with parameter and SE est as well as estimated weights.
 #'
 
-min.var.est = function(beta_SSL, beta_SL, resids_imp, resids_beta_SL,
-                       X_labeled, X_unlabeled, epsilon = NULL){
+SemiSupervisedMinVarRegression <- function(beta_SSL, beta_SL, resids_imp,
+                                           resids_beta_SL, X_labeled,
+                                           X_unlabeled, epsilon = NULL){
 
-  X_unlabeled.1 = cbind(1, rbind(X_labeled,X_unlabeled))
-  X_labeled.1 = cbind(1,X_labeled)
+  X_all <- cbind(1, rbind(X_labeled, X_unlabeled))
+  X_labeled_int <- cbind(1, X_labeled)
 
-  n.v = nrow(X_unlabeled.1)
-  n.t = nrow(X_labeled.1)
-  ones = c(1,1)
-  p = ncol(X_labeled)
-  weight = matrix(NA, p +1, 2)
+  n_all <- nrow(X_all)
+  n_labeled <- nrow(X_labeled_int)
+  ones <- c(1, 1)
+  p <- ncol(X_labeled)
+  weight <- matrix(NA, p + 1, 2)
 
-  if(is.null(epsilon)){epsilon = rep(0, p+1)};
+  if(is.null(epsilon)){epsilon = rep(0, p + 1)};
 
-  # compute minimum var estimator (note the residuals have been divided by mean of weights)
-
-  A = solve(t(X_unlabeled.1) %*% (X_unlabeled.1*c(dg.logit(X_unlabeled.1%*%beta_SSL))))*n.v
-  T_1 = A %*% t(X_labeled.1*c(resids_imp))
-  T_2 = A %*% t(X_labeled.1*c(resids_beta_SL))
+  # Compute the minimum variance estimator.
+  # Note: The residuals have been divided by mean of weights.
+  info_matrix <- solve(t(X_all) %*% (X_all * c(ExpitDerivative(X_all %*% beta_SSL))))
+  scaled_info_matrix <- info_matrix * n_all
+  T_1 <- A %*% t(X_labeled_int*c(resids_imp))
+  T_2 <- A %*% t(X_labeled_int*c(resids_beta_SL))
 
   for (i in 1:(p + 1)){
     mat = cbind(T_1[i,], T_2[i,])
-    cov = solve(t(mat)%*%mat  + epsilon[i]*diag(2))/n.t
+    cov = solve(t(mat)%*%mat  + epsilon[i]*diag(2))/n_labeled
     w2= t(ones) %*% cov %*% ones
     w1 = t(ones) %*% cov
     weight[i,] = w1/c(w2)
@@ -49,10 +51,10 @@ min.var.est = function(beta_SSL, beta_SL, resids_imp, resids_beta_SL,
   beta_SSL.w = beta_SSL*weight[,1] + weight[,2]*beta_SL
 
   # Standard error of minimum variance estimator.
-  A = solve(t(X_unlabeled.1) %*% (X_unlabeled.1*c(dg.logit(X_unlabeled.1%*%beta_SSL.w))))*n.v;
+  A = solve(t(X_all) %*% (X_all*c(dg.logit(X_all%*%beta_SSL.w))))*n_all;
   w.beta_SSL = diag(weight[,1]); w.beta_SL =  diag(weight[,2]);
-  resids.w = (w.beta_SSL %*% t(X_labeled.1*c(resids_imp)) +
-                w.beta_SL %*% t(X_labeled.1*resids_beta_SL))/n.t
+  resids.w = (w.beta_SSL %*% t(X_labeled_int*c(resids_imp)) +
+                w.beta_SL %*% t(X_labeled_int*resids_beta_SL))/n_labeled
   se.beta.w = sqrt(diag(A %*% (resids.w %*% t(resids.w)) %*% A))
 
 
