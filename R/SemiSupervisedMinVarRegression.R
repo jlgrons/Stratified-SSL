cc6281fdc8d7c818517d6c86e00bb607f7baeaa5
jlgrons <j.gronsbell@utoronto.ca>
2021-04-19 13:36:47 -0400

    Fix all equals signs.

--- R/SemiSupervisedMinVarRegression.R
+++ R/SemiSupervisedMinVarRegression.R
@@ -26,38 +26,38 @@ SemiSupervisedMinVarRegression <- function(beta_SSL, beta_SL, resids_imp,
   p <- ncol(X_labeled)
   weight <- matrix(NA, p + 1, 2)
 
-  if(is.null(epsilon)){epsilon = rep(0, p + 1)};
+  if(is.null(epsilon)){epsilon = rep(0, p + 1)}
 
   # Compute the minimum variance estimator.
   # Note: The residuals have been divided by mean of weights.
-  info_matrix <- solve(t(X_all) %*% (X_all * c(ExpitDerivative(X_all %*% beta_SSL))))
+  pred_prob_deriv <- c(ExpitDerivative(X_all %*% beta_SSL))
+  info_matrix <- solve(t(X_all) %*% (X_all * pred_prob_deriv))
   scaled_info_matrix <- info_matrix * n_all
-  T_1 <- A %*% t(X_labeled_int*c(resids_imp))
-  T_2 <- A %*% t(X_labeled_int*c(resids_beta_SL))
+
+  imp_IF <- scaled_info_matrix %*% t(X_labeled_int * c(resids_imp))
+  beta_SL_IF <- scaled_info_matrix %*% t(X_labeled_int * c(resids_beta_SL))
 
   for (i in 1:(p + 1)){
-    mat = cbind(T_1[i,], T_2[i,])
-    cov = solve(t(mat)%*%mat  + epsilon[i]*diag(2))/n_labeled
-    w2= t(ones) %*% cov %*% ones
-    w1 = t(ones) %*% cov
-    weight[i,] = w1/c(w2)
+    mat <- cbind(imp_IF[i,], beta_SL_IF[i,])
+    cov <- solve(t(mat)%*%mat  + epsilon[i]*diag(2))/n_labeled
+    w2 <- t(ones) %*% cov %*% ones
+    w1 <- t(ones) %*% cov
+    weight[i,] <- w1/c(w2)
     if (NA %in% weight[i,]){
       weight[i,] <- c(1, 0)
     }
-
   }
 
   # Componentwise minimum variance estimator.
-  beta_SSL.w = beta_SSL*weight[,1] + weight[,2]*beta_SL
+  beta_SSL.w <- beta_SSL*weight[,1] + weight[,2]*beta_SL
 
   # Standard error of minimum variance estimator.
-  A = solve(t(X_all) %*% (X_all*c(dg.logit(X_all%*%beta_SSL.w))))*n_all;
-  w.beta_SSL = diag(weight[,1]); w.beta_SL =  diag(weight[,2]);
-  resids.w = (w.beta_SSL %*% t(X_labeled_int*c(resids_imp)) +
+  A <- solve(t(X_all) %*% (X_all*c(dg.logit(X_all%*%beta_SSL.w))))*n_all;
+  w.beta_SSL <- diag(weight[,1]); w.beta_SL =  diag(weight[,2]);
+  resids.w <- (w.beta_SSL %*% t(X_labeled_int*c(resids_imp)) +
                 w.beta_SL %*% t(X_labeled_int*resids_beta_SL))/n_labeled
-  se.beta.w = sqrt(diag(A %*% (resids.w %*% t(resids.w)) %*% A))
+  se.beta.w <- sqrt(diag(A %*% (resids.w %*% t(resids.w)) %*% A))
 
-
-  return(list(beta = beta_SSL.w, weight= weight, se.est = se.beta.w))
+  return(list(beta = beta_SSL.w, weight = weight, se.est = se.beta.w))
 }
 
