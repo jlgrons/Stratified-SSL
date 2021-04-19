f8065c19eaaace876ec7efe871c1a0ed3316d49f
jlgrons <j.gronsbell@utoronto.ca>
2021-04-19 11:53:22 -0400

    Clean up ridge regression function.

--- R/RidgeRegression.R
+++ R/RidgeRegression.R
@@ -1,19 +1,29 @@
-# fits the penalized ridge model for semi-supervised estimate
-my.ridge = function(x, y, weights = NULL, lambda0 = 1e-04){
+# Updated: 2021-04-19
 
-  ## x: predictors
-  ## y: outcome
-  ## weights: sampling or resampling weights
+#' Ridge regression.
+#'
+#' @param X Covariate matrix.
+#' @param y Numeric outcome vector.
+#' @param weights Numeric vector of weights.
+#' @param lambda Penalization parameter.
+#' @param family Exponential family of interest.
+#' @export
+#' @return Vector containing regression coefficients.
+#'
+
+RidgeRegression <- function(X, y, weights = NULL, lambda = 1e-04,
+                            family = 'binomial'){
 
   if(is.null(weights)){weights = rep(1, length(y))}
 
-  # multiple lambdas to make sure convergence happens
-  gamma = tryCatch((coef(glmnet(x, y, weights = weights, alpha = 0,
-                                lambda = seq(lambda0, 100*lambda0, length.out = 100),
-                                family = 'binomial'))),
+  # Multiple lambdas to make sure convergence happens.
+  gamma = tryCatch((coef(glmnet(X, y, weights = weights, alpha = 0,
+                                lambda = seq(lambda, 100*lambda, length.out = 100),
+                                family = family))),
                    error = function(e) rep(NA, ncol(x) + 1 ))
 
-  # take the value at lambda0
+  # Take the value at specified value of lambda.
   gamma = gamma[, ncol(gamma)]
+
   return(gamma)
 }
