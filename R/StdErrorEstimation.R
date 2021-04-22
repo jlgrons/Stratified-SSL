c68c85c98cd9c1fcd1012ee63a3c70048c9e7c9b
jlgrons <j.gronsbell@utoronto.ca>
2021-04-22 09:47:07 -0400

    Clean standard error estimation function.

--- R/StdErrorEstimation.R
+++ R/StdErrorEstimation.R
@@ -1,24 +1,37 @@
-# computes a standard error estimate using the influence function of
-# solution to a simple estimating equation
-se.est = function(bhat, Yt, Xt, Xv, resid){
+# Updated: 2021-04-22
 
-  ## bhat: parameter estimate
-  ## Xt: covariates for labeled
-  ## Xv: covariates for unlabeled
-  ## Yt: labels
-  ## resids: vector of CV/non-CV residuals to compute score
+#' Standard error estimation based on influence function expansion.
+#'
+#' @param X_labeled Covariate matrix for labeled data set.
+#' @param X_unlabeled Covariate matrix for unlabeled data set.
+#' @param y Numeric outcome vector.
+#' @param beta Regression parameter estimate to compute the score function.
+#' @param residual Estimated residual.
+#' @export
+#' @return Standard error estimate.
+#'
 
-  Xt.1 = cbind(1,Xt); Xv.1 = cbind(1, rbind(Xt,Xv));
-  n.v = nrow(Xv.1); n.t = nrow(Xt.1);
+StdErrorEstimation <- function(X_labeled, X_unlabeled, y, beta, residual){
 
-  S =  t(Xt.1) %*% (Xt.1 * c(resid)^2)/n.t;
-  ddd = c(dg.logit(Xv.1%*%bhat))
-  ddd[which(is.na(ddd))] = 0
-  I = t(Xv.1) %*% (Xv.1 * ddd)/n.v;
+  X_labeled_intercept <- cbind(1, X_labaled)
+  X_all_intercept <- cbind(1, rbind(X_labeled, X_unlabeled))
 
-  A = solve(I);
-  vars =  A %*% S %*% A;
-  return(list(se = sqrt(diag(vars)/n.t), A = A))
+  n_all <- nrow(X_all_intercept)
+  n_labeled <- nrow(X_labeled_intercept)
+
+  score <-  t(X_labeled_intercept) %*% (
+    X_labeled_intercept * residual^2) / n_labeled
+
+  expit_deriv <- c(ExpitDerivative(X_all_intercept %*% bhat))
+  # Ask Molei about this.
+  expit_deriv[which(is.na(expit_deriv))] <- 0
+
+  information <- t(X_all_intercept) %*% (X_all_intercept * expit_deriv) / n_all
+  inverse_information <- solve(information)
+  variance_est <-  inverse_information %*% score %*% inverse_information
+
+  return(list(std_error = sqrt(diag(inverse_information) / n_labeled),
+              inverse_information = inverse_information))
 }
 
 
