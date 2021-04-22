93121a1e4acf021e474146c1e5cea877b0e43f71
jlgrons <j.gronsbell@utoronto.ca>
2021-04-22 07:14:17 -0400

    Finish cleaning apparent accuracy functions for semi-supervised setting.

--- R/SemiSupervisedAccuracy.R
+++ R/SemiSupervisedAccuracy.R
@@ -13,24 +13,34 @@
 #' @return resamp_weightector containing regression coefficients.
 #'
 
-SemiSupervisedApparentAccuracy <- function(basis_labeled, basis_unlabeled,
+SemiSupervisedAccuracy <- function(basis_labeled, basis_unlabeled,
                                            X_labeled, X_unlabaled, y, beta_SSL,
                                            beta_imp, weight = NULL,
-                                           threshold = 0.5)
+                                           threshold = NULL){
 
   if(is.null(weight)){weight <- rep(1, length(y))}
 
-  pred_prob_SSL_all <- Logit(cbind(1,X_all) %*% beta_ssl)
-  classification_SSL_all <- I(pred_prob_SSL_all > c)
+  X_all <- rbind(X_labeled, X_unlabeled)
+  basis_all <- rbind(basis_labeled, basis_unlabeled)
 
-  # Note: Used to be a typo here for beta SL.
-  pred_prob_SSL_labeled <- Logit(cbind(1,X_labeled) %*% beta_ssl)
-  classification_SSL_labeled <- I(pred_prob_SSL_labeled > c)
+  if(is.null(threshold)){
+    pred_prob_SSL_all <- Logit(cbind(1,X_all) %*% beta_SSL)
+    pred_prob_SSL_labeled <- Logit(cbind(1,X_labeled) %*% beta_SSL)
+  }else{
+    # Note: Used to be a typo here for beta SL.
+    pred_prob_SSL_all <- I(Logit(cbind(1,X_all) %*% beta_SSL) > threshold)
+    pred_prob_SSL_labeled <- I(Logit(cbind(1,X_labeled) %*% beta_SSL) > threshold)
+  }
 
-  imps_labeled <- cbind(1, basis_labeled)%*%beta_imp
+  imps_labeled <- cbind(1, basis_labeled) %*% beta_imp
+  imps_all <- cbind(1, basis_all) %*% beta_imp
 
-  refit_MSE <- glm(Yt~cbind(lp.t.ssl), offset = imps.t, family = 'binomial', weights = weight)$coeff
-  imps_MSE <- g.logit(cbind(1,pred_prob_SSL_all)%*%refit.p1 +  cbind(1, basis.x)%*%gamma)
-  MSE_SSL <- mean(imps.pe1 + (pred_prob_SSL_all - 2*imps.pe1)*pred_prob_SSL_all)
+  beta_refit <- glm(y ~ cbind(pred_prob_SSL_labeled), offset = imps_labeled,
+                   family = 'binomial', weights = weight)$coeff
+  imps_refit <- g.logit(cbind(1, pred_prob_SSL_all) %*% beta_refit +
+                          imps_all)
+  accuracy_SSL <- mean(imps_refit +
+                         (pred_prob_SSL_all - 2*imps_refit)*pred_prob_SSL_all)
 
+  return(accuracy_SSL)
 }
--- R/SemiSupervisedApparentAccuracy.R
+++ R/SemiSupervisedApparentAccuracy.R
@@ -8,7 +8,7 @@
 #' @param X_unlabeled Covariate matrix for unlabeled data set.
 #' @param y Numeric outcome vector.
 #' @param samp_prob Numeric vector of weights.
-#' @param weight Numeric vector of resampling weights.
+#' @param resamp_weight Numeric vector of resampling weights.
 #' @param threshold Threshold for overall misclassification rate.
 #' @export
 #' @return resamp_weightector containing regression coefficients.
@@ -21,25 +21,16 @@ SemiSupervisedApparentAccuracy <- function(basis_labeled, basis_unlabeled,
                                            threshold = 0.5){
 
   if(is.null(resamp_weight)){resamp_weight <- rep(1, length(y))}
-  weight <- resamp_weight/samp.prob/mean(resamp_weight/samp.prob)
-  X_all <- rbind(X_labeled, X_unlabeled)
+  weight <- resamp_weight/samp_prob/mean(resamp_weight/samp_prob)
 
-  pred_prob_SSL_all <- Logit(cbind(1,X_all) %*% beta_ssl)
-  classification_SSL_all <- I(pred_prob_SSL_all > c)
+  MSE_SSL <- SemiSupervisedAccuracy(basis_labeled, basis_unlabeled,
+                                    X_labeled, X_unlabaled, y, beta_SSL,
+                                    beta_imp, weight = weight, threshold = NULL)
 
-  # Note: Used to be a typo here for beta SL.
-  pred_prob_SSL_labeled <- Logit(cbind(1,X_labeled) %*% beta_ssl)
-  classification_SSL_labeled <- I(pred_prob_SSL_labeled > c)
+  OMR_SSL <- SemiSupervisedAccuracy(basis_labeled, basis_unlabeled,
+                                    X_labeled, X_unlabaled, y, beta_SSL,
+                                    beta_imp, weight = weight,
+                                    threshold = threshold)
 
-  imps_labeled <- cbind(1, basis_labeled)%*%beta_imp
-
-  refit_MSE <- glm(Yt~cbind(lp.t.ssl), offset = imps.t, family = 'binomial', weights = weight)$coeff
-  imps_MSE <- g.logit(cbind(1,pred_prob_SSL_all)%*%refit.p1 +  cbind(1, basis.x)%*%gamma)
-  MSE_SSL <- mean(imps.pe1 + (pred_prob_SSL_all - 2*imps.pe1)*pred_prob_SSL_all)
-
-  refit_OMR <- glm(Yt~cbind(lp.t.ssl.ind), offset = imps.t, family = 'binomial', weights = weight)$coeff
-  imps_OMR <- g.logit(cbind(1,classification_SSL_all)%*%refit.p2 +  cbind(1, basis.x)%*%gamma)
-  OMR_SSL <- mean(imps.pe2 + (classification_SSL_all - 2*imps.pe2)*classification_SSL_all)
-
-  return(list(mse_ssl = SSL_MSE, omr_sl = OMR_SSL))
+  return(list(mse_ssl = MSE_SSL, omr_ssl = OMR_SSL))
 }
