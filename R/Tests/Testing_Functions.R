0879bbc0e069812eb745c8d24a2d02ea8e535256
jlgrons <j.gronsbell@utoronto.ca>
2021-06-14 15:31:29 -0400

    fix testing function to allow for differences in column names... annoying

--- R/Tests/Testing_Functions.R
+++ R/Tests/Testing_Functions.R
@@ -2,6 +2,11 @@ run_data_gen_test <- function(old_data, new_data){
 
   signal_test <- all.equal(old_data$b0[-1], new_data$signal)
 
+  colnames(old_data$Xt) <- colnames(new_data$covariates_lab)
+  colnames(old_data$Xv) <- colnames(new_data$covariates_unlab)
+  colnames(old_data$Xr) <- colnames(new_data$covariates_random_samp)
+  colnames(old_data$Xvr) <- colnames(new_data$covariates_unlab_random_samp)
+
   cov_test <- all.equal(old_data$X0, new_data$covariates)
   lab_cov_test <- all.equal(old_data$Xt, new_data$covariates_lab)
   unlab_cov_test <- all.equal(old_data$Xv, new_data$covariates_unlab)
@@ -9,7 +14,7 @@ run_data_gen_test <- function(old_data, new_data){
   rs_unlab_cov_test <- all.equal(old_data$Xvr, new_data$covariates_unlab_random_samp)
 
   lab_outcome_test <- all.equal(old_data$Yt, new_data$Y_lab)
-  outcome_test <- all.equal(old_data$Y, new_data$Y)
+  outcome_test <- all.equal(c(old_data$Y), c(new_data$Y))
   rs_lab_outcome_test <- all.equal(old_data$Yr, new_data$Y_random_samp)
 
   lab_strat_test <- all.equal(old_data$St, new_data$S_lab)
