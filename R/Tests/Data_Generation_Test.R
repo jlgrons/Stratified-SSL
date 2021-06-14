4bde94e75d966a9caaf2e85de4e062e1fc669772
jlgrons <j.gronsbell@utoronto.ca>
2021-06-14 15:33:57 -0400

    Update testing code.

--- R/Tests/Data_Generation_Test.R
+++ R/Tests/Data_Generation_Test.R
@@ -86,14 +86,62 @@ sum(unlist(test_result)) == length(test_result)
 ## IC: Incorrect Prediction and Imputation Model ##
 ###################################################
 
+# OLD CODE
+setwd('~/Desktop/Stratified-SSL/R/Original Code')
+source('function.R')
+set.seed(92047)
+# Sample size of label data
+n.t <- 400
+# Matrix correlation
+rho <- 0.4
+# Sample size of unlabeled data
+N <- 20000
+# Number of covariates
+p <- 10
+# Number of stratum
+strata.num <- 2
+# Generate Data
+old_data <- data_gen(n.t, N, p, rho, model.spec = 'II', strata_num = strata.num)
 
-## Simulations for the Supplementary Materials ##
+# NEW CODE
+setwd('~/Desktop/Stratified-SSL/R')
+source('DataGeneration.R')
+source('HelperFunctions.R')
+set.seed(92047)
+n_lab <- 400
+n_unlab <- 20000
+p <- 10
+rho <- 0.4
+num_strata <- 2
+new_data <- DataGeneration(n_lab, n_unlab, p, rho, signal = c(1, 1, 0.5, 0.5),
+                           model_specification = 'outcome_incorrect_imputation_incorrect',
+                           num_strata = num_strata)
 
-## GM
+# RUN TEST
+test_result <- run_data_gen_test(old_data, new_data)
+# Check if all tests passed
+sum(unlist(test_result)) == length(test_result)
 
 
-## IC1
+# __________________________________________________________________________ #
+
+## Simulations for the Main Manuscript ##
+
+#############################
+## Gaussian Mixture Model ##
+############################
 
 
-## II1
+
+###########################################################
+## IC: Incorrect Prediction and Correct Imputation Model ##
+###########################################################
+
+
+###################################################
+## IC: Incorrect Prediction and Imputation Model ##
+###################################################
+
+
+
 
