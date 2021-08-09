1c87b6d202ea65883a6f3522af633efe8405ec5b
jlgrons <j.gronsbell@utoronto.ca>
2021-08-09 18:40:09 -0400

    Update analysis file.

--- R/Original Code/response_main_IC_analyze.R
+++ R/Original Code/response_main_IC_analyze.R
@@ -1,9 +1,9 @@
 setwd('~/Desktop')
 n.t <- 200
-my_beta_true_200 <- read.table( paste0(n.t, "-beta_true.csv"))
-my_beta_sl_200 <- read.table( paste0(n.t, "-beta_sl.csv"))
-my_beta_ssl_200 <- read.table( paste0(n.t, "-beta_ssl.csv"))
-my_beta_ssl2_200 <- read.table( paste0(n.t, "-beta_ssl2.csv"))
+my_beta_true_200 <- as.matrix(read.table( paste0(n.t, "-beta_true.csv")))
+my_beta_sl_200 <- as.matrix(read.table( paste0(n.t, "-beta_sl.csv")))
+my_beta_ssl_200 <- as.matrix(read.table( paste0(n.t, "-beta_ssl.csv")))
+my_beta_ssl2_200 <- as.matrix(read.table( paste0(n.t, "-beta_ssl2.csv")))
 
 my_beta_true_200_m <- colMeans(my_beta_true_200)
 my_beta_sl_200_m <- colMeans(my_beta_sl_200)
@@ -15,9 +15,43 @@ my_beta_sl_200_m
 my_beta_ssl_200_m
 my_beta_ssl2_200_m
 
+my_beta_true_200_s <- colSds(my_beta_true_200)
+my_beta_sl_200_s <- colSds(my_beta_sl_200)
+my_beta_ssl_200_s <- colSds(my_beta_ssl_200)
+my_beta_ssl2_200_s <- colSds(my_beta_ssl2_200)
+
+my_beta_true_200_s
+my_beta_sl_200_s
+my_beta_ssl_200_s
+my_beta_ssl2_200_s
+
+
+
 setwd('~/Desktop')
 n.t <- 400
-my_beta_true_400 <- read.table( paste0(n.t, "-beta_true.csv"))
-my_beta_sl_400 <- read.table( paste0(n.t, "-beta_sl.csv"))
-my_beta_ssl_400 <- read.table( paste0(n.t, "-beta_ssl.csv"))
-my_beta_ssl2_400 <- read.table( paste0(n.t, "-beta_ssl2.csv"))
+my_beta_true_400 <- as.matrix(read.table( paste0(n.t, "-beta_true.csv")))
+my_beta_sl_400 <- as.matrix(read.table( paste0(n.t, "-beta_sl.csv")))
+my_beta_ssl_400 <- as.matrix(read.table( paste0(n.t, "-beta_ssl.csv")))
+my_beta_ssl2_400 <- as.matrix(read.table( paste0(n.t, "-beta_ssl2.csv")))
+
+
+my_beta_true_400_m <- colMeans(my_beta_true_400)
+my_beta_sl_400_m <- colMeans(my_beta_sl_400)
+my_beta_ssl_400_m <- colMeans(my_beta_ssl_400)
+my_beta_ssl2_400_m <- colMeans(my_beta_ssl2_400)
+
+my_beta_true_400_m
+my_beta_sl_400_m
+my_beta_ssl_400_m
+my_beta_ssl2_400_m
+
+my_beta_true_400_s <- colSds(my_beta_true_400)
+my_beta_sl_400_s <- colSds(my_beta_sl_400)
+my_beta_ssl_400_s <- colSds(my_beta_ssl_400)
+my_beta_ssl2_400_s <- colSds(my_beta_ssl2_400)
+
+my_beta_true_400_s
+my_beta_sl_400_s
+my_beta_ssl_400_s
+my_beta_ssl2_400_s
+
