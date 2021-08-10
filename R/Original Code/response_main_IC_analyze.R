769b719affccf1ce1204737df93e00e527cc1df4
jlgrons <j.gronsbell@utoronto.ca>
2021-08-10 10:02:08 -0400

    Create the table.

--- R/Original Code/response_main_IC_analyze.R
+++ R/Original Code/response_main_IC_analyze.R
@@ -18,7 +18,7 @@ my_beta_ssl2_200_s <- colSds(my_beta_ssl2_200)
 bias_sl_S2 <- my_beta_sl_200_m - my_beta_true_200_m
 bias_ssl_S2 <- my_beta_ssl_200_m - my_beta_true_200_m
 bias_ssl2_S2 <- my_beta_ssl2_200_m - my_beta_true_200_m
-bias_200_S2 <- round(cbind(bias_sl_S2, bias_ssl_S2, bias_ssl2_S2), 2)
+bias_200_S2 <- cbind(bias_sl_S2, bias_ssl_S2, bias_ssl2_S2)
 bias_200_S2
 
 n.t <- 400
@@ -42,7 +42,7 @@ my_beta_ssl2_400_s <- colSds(my_beta_ssl2_400)
 bias_sl_S2 <- my_beta_sl_400_m - my_beta_true_400_m
 bias_ssl_S2 <- my_beta_ssl_400_m - my_beta_true_400_m
 bias_ssl2_S2 <- my_beta_ssl2_400_m - my_beta_true_400_m
-bias_400_S2 <- round(cbind(bias_sl_S2, bias_ssl_S2, bias_ssl2_S2), 2)
+bias_400_S2 <- cbind(bias_sl_S2, bias_ssl_S2, bias_ssl2_S2)
 bias_400_S2
 
 setwd('~/Desktop/stratSSL_Revision_Results/new_result_S4')
@@ -65,7 +65,7 @@ my_beta_ssl2_200_s <- colSds(my_beta_ssl2_200)
 bias_sl_S4 <- my_beta_sl_200_m - my_beta_true_200_m
 bias_ssl_S4 <- my_beta_ssl_200_m - my_beta_true_200_m
 bias_ssl2_S4 <- my_beta_ssl2_200_m - my_beta_true_200_m
-bias_200_S4 <- round(cbind(bias_sl_S4, bias_ssl_S4, bias_ssl2_S4), 2)
+bias_200_S4 <- cbind(bias_sl_S4, bias_ssl_S4, bias_ssl2_S4)
 bias_200_S4
 
 n.t <- 400
@@ -89,8 +89,30 @@ my_beta_ssl2_400_s <- colSds(my_beta_ssl2_400)
 bias_sl_S4 <- my_beta_sl_400_m - my_beta_true_400_m
 bias_ssl_S4 <- my_beta_ssl_400_m - my_beta_true_400_m
 bias_ssl2_S4 <- my_beta_ssl2_400_m - my_beta_true_400_m
-bias_400_S4 <- round(cbind(bias_sl_S4, bias_ssl_S4, bias_ssl2_S4), 2)
+bias_400_S4 <- cbind(bias_sl_S4, bias_ssl_S4, bias_ssl2_S4)
 bias_400_S4
+
+round(bias_400_S2,4)
+round(bias_400_S4,4)
+
+
+
+# create the table
+options("scipen" = 100, "digits" = 5)
+
+cbind(round(bias_200_S2[, 3]  - bias_200_S2[, 2], 4),
+      round(bias_400_S2[, 3]  - bias_400_S2[, 2], 4),
+      round(bias_200_S4[, 3]  - bias_200_S4[, 2], 4),
+      round(bias_400_S4[, 3]  - bias_400_S4[, 2], 4)
+)
+
+
+library(xtable)
+xtable()
+
+
+
+
 
 
 
