3afe7d233b264bc0e8b110ff2ed6abdf34bc7d15
jlgrons <j.gronsbell@utoronto.ca>
2021-08-10 09:50:20 -0400

    Update code for both results.

--- R/Original Code/response_main_IC_analyze.R
+++ R/Original Code/response_main_IC_analyze.R
@@ -1,4 +1,4 @@
-setwd('~/Desktop/new_result_S4')
+setwd('~/Desktop/stratSSL_Revision_Results/old_result_S2')
 n.t <- 200
 my_beta_true_200 <- as.matrix(read.table( paste0(n.t, "-beta_true.csv")))
 my_beta_sl_200 <- as.matrix(read.table( paste0(n.t, "-beta_sl.csv")))
@@ -10,26 +10,16 @@ my_beta_sl_200_m <- colMeans(my_beta_sl_200)
 my_beta_ssl_200_m <- colMeans(my_beta_ssl_200)
 my_beta_ssl2_200_m <- colMeans(my_beta_ssl2_200)
 
-my_beta_true_200_m
-my_beta_sl_200_m
-my_beta_ssl_200_m
-my_beta_ssl2_200_m
-
 my_beta_true_200_s <- colSds(my_beta_true_200)
 my_beta_sl_200_s <- colSds(my_beta_sl_200)
 my_beta_ssl_200_s <- colSds(my_beta_ssl_200)
 my_beta_ssl2_200_s <- colSds(my_beta_ssl2_200)
 
-my_beta_true_200_s
-my_beta_sl_200_s
-my_beta_ssl_200_s
-my_beta_ssl2_200_s
-
-bias_sl <- my_beta_sl_200_m - my_beta_true_200_m
-bias_ssl <- my_beta_ssl_200_m - my_beta_true_200_m
-bias_ssl2 <- my_beta_ssl2_200_m - my_beta_true_200_m
-bias_200 <- round(cbind(bias_sl, bias_ssl, bias_ssl2), 2)
-bias_200
+bias_sl_S2 <- my_beta_sl_200_m - my_beta_true_200_m
+bias_ssl_S2 <- my_beta_ssl_200_m - my_beta_true_200_m
+bias_ssl2_S2 <- my_beta_ssl2_200_m - my_beta_true_200_m
+bias_200_S2 <- round(cbind(bias_sl_S2, bias_ssl_S2, bias_ssl2_S2), 2)
+bias_200_S2
 
 n.t <- 400
 my_beta_true_400 <- as.matrix(read.table( paste0(n.t, "-beta_true.csv")))
@@ -43,25 +33,64 @@ my_beta_sl_400_m <- colMeans(my_beta_sl_400)
 my_beta_ssl_400_m <- colMeans(my_beta_ssl_400)
 my_beta_ssl2_400_m <- colMeans(my_beta_ssl2_400)
 
-my_beta_true_400_m
-my_beta_sl_400_m
-my_beta_ssl_400_m
-my_beta_ssl2_400_m
 
 my_beta_true_400_s <- colSds(my_beta_true_400)
 my_beta_sl_400_s <- colSds(my_beta_sl_400)
 my_beta_ssl_400_s <- colSds(my_beta_ssl_400)
 my_beta_ssl2_400_s <- colSds(my_beta_ssl2_400)
 
-my_beta_true_400_s
-my_beta_sl_400_s
-my_beta_ssl_400_s
-my_beta_ssl2_400_s
+bias_sl_S2 <- my_beta_sl_400_m - my_beta_true_400_m
+bias_ssl_S2 <- my_beta_ssl_400_m - my_beta_true_400_m
+bias_ssl2_S2 <- my_beta_ssl2_400_m - my_beta_true_400_m
+bias_400_S2 <- round(cbind(bias_sl_S2, bias_ssl_S2, bias_ssl2_S2), 2)
+bias_400_S2
 
-bias_sl <- my_beta_sl_400_m - my_beta_true_400_m
-bias_ssl <- my_beta_ssl_400_m - my_beta_true_400_m
-bias_ssl2 <- my_beta_ssl2_400_m - my_beta_true_400_m
-bias_400 <- round(cbind(bias_sl, bias_ssl, bias_ssl2), 2)
-bias_400
+setwd('~/Desktop/stratSSL_Revision_Results/new_result_S4')
+n.t <- 200
+my_beta_true_200 <- as.matrix(read.table( paste0(n.t, "-beta_true.csv")))
+my_beta_sl_200 <- as.matrix(read.table( paste0(n.t, "-beta_sl.csv")))
+my_beta_ssl_200 <- as.matrix(read.table( paste0(n.t, "-beta_ssl.csv")))
+my_beta_ssl2_200 <- as.matrix(read.table( paste0(n.t, "-beta_ssl2.csv")))
+
+my_beta_true_200_m <- colMeans(my_beta_true_200)
+my_beta_sl_200_m <- colMeans(my_beta_sl_200)
+my_beta_ssl_200_m <- colMeans(my_beta_ssl_200)
+my_beta_ssl2_200_m <- colMeans(my_beta_ssl2_200)
+
+my_beta_true_200_s <- colSds(my_beta_true_200)
+my_beta_sl_200_s <- colSds(my_beta_sl_200)
+my_beta_ssl_200_s <- colSds(my_beta_ssl_200)
+my_beta_ssl2_200_s <- colSds(my_beta_ssl2_200)
+
+bias_sl_S4 <- my_beta_sl_200_m - my_beta_true_200_m
+bias_ssl_S4 <- my_beta_ssl_200_m - my_beta_true_200_m
+bias_ssl2_S4 <- my_beta_ssl2_200_m - my_beta_true_200_m
+bias_200_S4 <- round(cbind(bias_sl_S4, bias_ssl_S4, bias_ssl2_S4), 2)
+bias_200_S4
+
+n.t <- 400
+my_beta_true_400 <- as.matrix(read.table( paste0(n.t, "-beta_true.csv")))
+my_beta_sl_400 <- as.matrix(read.table( paste0(n.t, "-beta_sl.csv")))
+my_beta_ssl_400 <- as.matrix(read.table( paste0(n.t, "-beta_ssl.csv")))
+my_beta_ssl2_400 <- as.matrix(read.table( paste0(n.t, "-beta_ssl2.csv")))
+
+
+my_beta_true_400_m <- colMeans(my_beta_true_400)
+my_beta_sl_400_m <- colMeans(my_beta_sl_400)
+my_beta_ssl_400_m <- colMeans(my_beta_ssl_400)
+my_beta_ssl2_400_m <- colMeans(my_beta_ssl2_400)
+
+
+my_beta_true_400_s <- colSds(my_beta_true_400)
+my_beta_sl_400_s <- colSds(my_beta_sl_400)
+my_beta_ssl_400_s <- colSds(my_beta_ssl_400)
+my_beta_ssl2_400_s <- colSds(my_beta_ssl2_400)
+
+bias_sl_S4 <- my_beta_sl_400_m - my_beta_true_400_m
+bias_ssl_S4 <- my_beta_ssl_400_m - my_beta_true_400_m
+bias_ssl2_S4 <- my_beta_ssl2_400_m - my_beta_true_400_m
+bias_400_S4 <- round(cbind(bias_sl_S4, bias_ssl_S4, bias_ssl2_S4), 2)
+bias_400_S4
+
 
 
