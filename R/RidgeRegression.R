da52d0b7d5f8975f8d1188c52adbe159a26cf381
jlgrons <j.gronsbell@utoronto.ca>
2021-08-30 14:21:28 -0400

    Fix typo.

--- R/RidgeRegression.R
+++ R/RidgeRegression.R
@@ -20,7 +20,7 @@ RidgeRegression <- function(X, y, weights = NULL, lambda = 1e-04,
   gamma = tryCatch((coef(glmnet(X, y, weights = weights, alpha = 0,
                                 lambda = seq(lambda, 100*lambda, length.out = 100),
                                 family = family))),
-                   error = function(e) rep(NA, ncol(x) + 1 ))
+                   error = function(e) rep(NA, ncol(X) + 1 ))
 
   # Take the value at specified value of lambda.
   gamma = gamma[, ncol(gamma)]
